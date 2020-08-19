1fb58c011eb7992fca9cb1f3f03e985bb978e312
liukenan <liukenan@meetstarinc.com>
2020-08-19 17:32:13 +0800

    增加基本配置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-
-
+import './basic.styl'
 function App() {
   return (
     <div className="App">
+      
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +20,10 @@ function App() {
           Learn React
         </a>
       </header> */}
+
+      
+      
+      <header />
     </div>
   );
 }
